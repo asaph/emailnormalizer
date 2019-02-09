0c2b3b3e5cedc0272854d5d3244d0a07e4d7ec4b
asaph <ace@dontletgo.com>
2019-02-09 14:39:00 -0800

    Add a static map of EmailNormalizer implementations indexed by domain

--- src/main/java/org/asaph/emailnormalizer/EmailNormalizer.java
+++ src/main/java/org/asaph/emailnormalizer/EmailNormalizer.java
@@ -1,5 +1,13 @@
 package org.asaph.emailnormalizer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class EmailNormalizer {
+    public final static Map<String, EmailNormalizer> DOMAIN_NORMALIZER_MAP = new HashMap<>();
+    static {
+        DOMAIN_NORMALIZER_MAP.put("gmail.com", new GmailNormalizer());
+    }
+
     public abstract String normalize(String emailAddress);
 }
