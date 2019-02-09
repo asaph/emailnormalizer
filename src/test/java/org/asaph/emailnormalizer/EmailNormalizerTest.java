ca88dd50edf499ee85efc2cce0605ececb03aa37
asaph <ace@dontletgo.com>
2019-02-09 15:33:50 -0800

    Add static imports in unit test

--- src/test/java/org/asaph/emailnormalizer/EmailNormalizerTest.java
+++ src/test/java/org/asaph/emailnormalizer/EmailNormalizerTest.java
@@ -1,6 +1,7 @@
 package org.asaph.emailnormalizer;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.asaph.emailnormalizer.EmailNormalizer.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,9 +9,9 @@ class EmailNormalizerTest {
 
     @Test
     void testNormalizeEmail() {
-        assertEquals("thisis@gmail.com", EmailNormalizer.normalizeEmail("this.is+a.test@gmail.com"));
+        assertEquals("thisis@gmail.com", normalizeEmail("this.is+a.test@gmail.com"));
         String email = "this.is+a.test@example.com";
-        assertSame(email, EmailNormalizer.normalizeEmail(email));
+        assertSame(email, normalizeEmail(email));
     }
 
     @Test
@@ -21,11 +22,11 @@ class EmailNormalizerTest {
 
     @Test
     void testGetDomain() {
-        assertEquals("gmail.com", EmailNormalizer.getDomain("this.is.a.test@gmail.com"));
+        assertEquals("gmail.com", getDomain("this.is.a.test@gmail.com"));
     }
 
     @Test
     void testGetUsername() {
-        assertEquals("this.is.a.test", EmailNormalizer.getUsername("this.is.a.test@gmail.com"));
+        assertEquals("this.is.a.test", getUsername("this.is.a.test@gmail.com"));
     }
 }
